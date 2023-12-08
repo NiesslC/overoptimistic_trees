b77ccf4ee65fa0c3b3c3302e625148bd8a508ed8
Christina Niessl <christina.niessl@gmx.de>
2023-12-08 11:02:02 +0100

    Added PipeOps for preproc.drop.targetout and preproc.drop.iposca

--- 02_code/_fcts_pipeops.R
+++ 02_code/_fcts_pipeops.R
@@ -71,3 +71,70 @@ PipeOpPreprocTarget = R6::R6Class("PipeOpPreprocTarget",
                                        )
 )
 mlr_pipeops$add("preproc.target", PipeOpPreprocTarget)
+
+
+
+PipeOpDropTargetOut = R6::R6Class("PipeOpDropTargetOut",
+                           inherit = mlr3pipelines::PipeOpTaskPreproc,
+                           public = list(
+                             initialize = function(id = "preproc.drop.targetout", param_vals = list()) {
+                               ps = ParamSet$new(params = list(
+                                 ParamFct$new("option", levels = c("A", "B", "C", "D"))
+                               ))
+                               ps$values = list(option = "A")
+                               super$initialize(id = id, param_set = ps, 
+                                                param_vals = param_vals)
+                             }
+                           ),
+                           
+                           private = list(
+                             .train_task = function(task) {
+                               self$state = list(
+                                 threshold = preprocess_drop_targetout_fct(target_values = task$data(cols = task$target_names),
+                                                                    option=self$param_set$values$option)
+                               )
+                               exclude =  task$data(cols = task$target_names) > self$state$threshold
+                               task$filter(task$row_ids[!exclude])
+                             },
+                             
+                             .predict_task = function(task) {
+                               exclude =  task$data(cols = task$target_names) > self$state$threshold
+                               task$filter(task$row_ids[!exclude])
+                               
+                             }
+                           )
+)
+mlr_pipeops$add("preproc.drop.targetout", PipeOpDropTargetOut)
+
+
+PipeOpDropIposCa = R6::R6Class("PipeOpDropIposCa",
+                           inherit = mlr3pipelines::PipeOpTaskPreproc,
+                           public = list(
+                             initialize = function(id = "preproc.drop.iposca", param_vals = list()) {
+                               ps = ParamSet$new(params = list(
+                                 ParamFct$new("option", levels = c("A", "B", "C", "D", "E", "F", "G", "H"))
+                               ))
+                               ps$values = list(option = "A")
+                               super$initialize(id = id, param_set = ps, 
+                                                param_vals = param_vals)
+                             }
+                           ),
+                           
+                           private = list(
+                             .train_task = function(task) {
+                               self$state = list(
+                                 threshold = preprocess_drop_iposca_fct(option=self$param_set$values$option)
+                               )
+                               exclude =  (task$data() %>% select(contains("ipos")) %>% 
+                                             mutate(sum_ca = rowSums(. == "cannot assess")) %>% .$sum_ca) >= self$state$threshold
+                               task$filter(task$row_ids[!exclude])
+                             },
+                             .predict_task = function(task) {
+                               exclude =  (task$data() %>% select(contains("ipos")) %>% 
+                                             mutate(sum_ca = rowSums(. == "cannot assess")) %>% .$sum_ca) >= self$state$threshold
+                               task$filter(task$row_ids[!exclude])
+                             }
+                           )
+)
+
+mlr_pipeops$add("preproc.drop.iposca", PipeOpDropIposCa)
