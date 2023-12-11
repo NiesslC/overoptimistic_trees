f5d5bc31a81f0c1101270860a0851eacd93fbabc
Christina Niessl <christina.niessl@gmx.de>
2023-12-11 18:13:57 +0100

    moved all of fixed preprocessing to 01_script_preprocessing_fixed.R

--- 02_code/02_script_optimization_simulation.R
+++ 02_code/02_script_optimization_simulation.R
@@ -11,9 +11,9 @@ library(paradox)
 library(rpart.plot)
 
 
-load("./01_data/data_all.RData")
-source("./02_code/_fcts_preprocessing.R")
-source("./02_code/_fcts_pipeops.R")
+load("./01_data/data_phaselevel.RData")
+source("./02_code/_fcts_preproc.R")
+source("./02_code/_fcts_preproc2pipeop.R")
 source("./02_code/_fcts_trees.R")
 source("./02_code/_fcts_optim.R")
 
@@ -25,9 +25,10 @@ preproc_hp_set = list("preproc.target" = c("A", "B", "C"),
                       "preproc.drop.targetout" = c("A", "B", "C", "D"),
                       "preproc.drop.iposca" = c("A", "B", "C", "D", "E", "F", "G", "H"),
                       "preproc.feature.ipos" = c("A", "B", "C", "D", "E"),
-                      "preproc.feature.age" = c("A", "B"))
+                      "preproc.feature.age" = c("A", "B"),
+                      "preproc.feature.akps" = c("A", "B"))
 # Order for stepwise optimization 
-preproc_hp_stepopt_order = c("preproc.target", "preproc.feature.ipos", "preproc.feature.age",
+preproc_hp_stepopt_order = c("preproc.target", "preproc.feature.ipos", "preproc.feature.age", "preproc.feature.akps",
                              "preproc.drop.targetout", "preproc.drop.iposca")
 setting_param = "sapv"
 eval_criterion = "regr.rsq" 
@@ -68,48 +69,17 @@ tuning_parameters = list(
 ##### Repeat the following code n_rep times ####
 
 # 1. Select specified setting ----
-data = data %>% filter(setting == setting_param)
+data_phaselevel = data_phaselevel %>% filter(setting == setting_param)
 # 1. Allocation of train/test ----
-id_train = data %>% distinct(companion_id,.keep_all = TRUE) %>%  
+id_train = data_phaselevel %>% distinct(companion_id,.keep_all = TRUE) %>%  
   group_by(team_id) %>% # to ensure sampling will not change when adding new datasets
   slice_sample(prop = 0.5) %>% .$companion_id
-datasim_train = data %>% filter(companion_id %in% id_train) %>% mutate(allocation="train")
-datasim_test = data %>% filter(!companion_id %in% id_train) %>% mutate(allocation="test")
+datasim_train = data_phaselevel %>% filter(companion_id %in% id_train) %>% mutate(allocation="train")
+datasim_test = data_phaselevel %>% filter(!companion_id %in% id_train) %>% mutate(allocation="test")
 datasim = bind_rows(datasim_train, datasim_test)
 rm(id_train, datasim_train, datasim_test)
 
-# 2. Correction of first day of first phase using first day of next phases ----
-# (apply on whole data set but only use TRAIN for calculation)
-# (separately for all three possible targets)
 
-cf1 = preprocess_get_firstdaycorrect_fct(data = datasim %>% filter(allocation == "train"), target_name = target_names[1])
-datasim = preprocess_apply_firstdaycorrect_fct(data = datasim, target_name = target_names[1], correction_factors = cf1)
-
-cf2 = preprocess_get_firstdaycorrect_fct(data = datasim %>% filter(allocation == "train"), target_name = target_names[2])
-datasim = preprocess_apply_firstdaycorrect_fct(data = datasim, target_name = target_names[2], correction_factors = cf2)
-
-cf3 = preprocess_get_firstdaycorrect_fct(data = datasim %>% filter(allocation == "train"), target_name = target_names[3])
-datasim = preprocess_apply_firstdaycorrect_fct(data = datasim, target_name = target_names[3], correction_factors = cf3)
-rm(cf1, cf2, cf3)
-
-# 3. Get data on phase level ----
-# (remove variables varying within phase and only keep distinct values and make sure there is 
-# exactly one row per patient and phase)
-datasim = datasim %>% select(-date, -time, -datetime, -minutes, -cost, -cost_exclsys) %>% distinct()
-stopifnot(nrow(datasim %>% distinct(companion_id, grp)) == nrow(datasim))
-
-# 4. Removal of missing values (fixed) ----
-# - ipos_ variables: remove phases with NAs in ipos variables
-datasim = datasim %>% filter_at(vars(starts_with("ipos_")), all_vars(!is.na(.)))
-# - cogn_ variables: remove phases with NA in cogn variables
-datasim = datasim %>% filter_at(vars(starts_with("cogn_")), all_vars(!is.na(.)))
-# - akps: remove phases with NA or 'cannot assess' 
-datasim = datasim %>% filter(!is.na(akps) & akps != "cannot assess")
-
-# 5. Drop unused levels and convert team and companion id to factor ----
-datasim = datasim %>% droplevels()
-datasim$id = factor(datasim$companion_id)
-datasim$team_name = factor(datasim$team_id) 
 
 # 6. Start optimization ----
 ## Split train and test data ----
@@ -117,7 +87,8 @@ data_train = datasim %>% filter(allocation == "train")
 data_test = datasim %>% filter(allocation == "test")
 ## Specify target ----
 # only select possible targets, features, and id because per default the learner uses all variables in data set as features 
-data_train = data_train %>% select(targetvar, all_of(target_names), all_of(feature_names_raw), companion_id) 
+#data_train = data_train %>% select(targetvar, contains(c("sum_cost", "sum_minutes")), all_of(feature_names_raw), companion_id) 
+data_train = data_train %>% select(-allocation) 
 # specify companion_id as grouping variable bc observations from same id should not be split when resampling
 task = as_task_regr(data_train, target = "targetvar")
 # = group by id 
@@ -163,17 +134,12 @@ t4 = generate_and_eval_tree_fct(procedure = "preproc.hp.tune_algo.hp.tune",
                                 tuning_parameters = tuning_parameters)
 
 
-
-
-
 # next 
-####commit!
 ####mlr3 aussage bzgl. optimistic algorithm (now really needed) nochmal checken
 
 # nested resampling +
 # default hp
-# evtl. unnötige fehler löschen
-
+# evtl. unnötige errors löschen
 
 # schlimm dass nicht gleiche splits bei a)tunen (falls mehrere algos) und b) bei nested resampling stepwise
 
@@ -183,28 +149,27 @@ t4 = generate_and_eval_tree_fct(procedure = "preproc.hp.tune_algo.hp.tune",
 
 
 ###########################
+# evtl summarise info loswerden
+# add learner_graph$model$regr.rpart$train_task info
 #formula checken rpart!
 # falls mehrere learner: schlimm wenn auf unterschiedlichen splits getuned?
 # seeeeed!!!
-#mlr_pipeops_mutate
-# target :mlr_pipeops_updatetarget or mlr_pipeops_targetmutate ?
-# ipos NA/"missing entfernen" + für akps auch cannot assess entferenen
+# - verhalten der algorithmen bei missing values (auch bzgl. fixfactors)
+# - schauen dass bei target wirklich nicht benötigte target vars weggelassen in task
+# pipelines final entscheiden
+# feature names explizit bei task angeben, müssen noch variablen ausgeschlossen werden?
+#   evlt preproc die alle removed außer feature target und grouping
 ###
-# -for input in mlr3 prüfen dass keine NAs
 # - irgendwo sicherstellen dass in data_train und data_test dieselben levels vorhanden sind
 # ordinale vars nicht vergessen
-# -outlier removal könnte man auch stetig machen
-# ganz am schluss droplevels?
 # -fct descriptions
 # - document id_train irgendwo
 # - welche variables noch entfernt außer dry mouth
 # - setting auswahl parameter
 # - check dass alle die sein sollen ordinal 
-# - check dass kein NA + kein cannot assess in den daten 
-# - evtl. schnellere alternative für sysvar rlang 
-# - verhalten der algorithmen bei missing values (auch bzgl. fixfactors)
-# - man könnte manche hyperparameter auch stetig machen
-# - evtl cutoff werte für ipos cannot assess noch ändern (damit nicht zu viel entfernt)
+# - evtl auch different sample size for simulation
+# - falls extern kann keine correctur verwendet werden
+# - man könnte manche hyperparameter auch stetig machen (zB outlier removal)
 # - reihenfolge der pipelines beachten (zb select ipos_ kann probleme machen wenn nach transf. angewendet)
 #####################################################
 
