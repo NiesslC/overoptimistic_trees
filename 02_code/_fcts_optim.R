4715e97e5ba40e6d5536cd5c2f734d4797062e09
Christina Niessl <christina.niessl@gmx.de>
2024-01-01 19:34:57 +0100

    added "preproc.hp.default_algo.hp.tune" procedure; added name of preproc_hp_searchspace (previous_ preproc_hp_set)

--- 02_code/_fcts_optim.R
+++ 02_code/_fcts_optim.R
@@ -1,5 +1,5 @@
-generate_and_eval_tree_fct = function(procedure,
-                                      preproc_hp_set,
+get_tree_and_error_fct = function(procedure,
+                                      preproc_hp_searchspace,
                                       task,
                                       data_test, 
                                       eval_criterion, 
@@ -19,19 +19,19 @@ generate_and_eval_tree_fct = function(procedure,
     }
     
     # Initialize preprocessing hyperparameter setting (= defaults = all options set to "A")
-    current_preproc_hp = list("preproc.target" = preproc_hp_set$preproc.target[[1]],
-                              "preproc.drop.targetout" = preproc_hp_set$preproc.drop.targetout[[1]],
-                              "preproc.drop.iposca" = preproc_hp_set$preproc.drop.iposca[[1]],
-                              "preproc.feature.ipos" = preproc_hp_set$preproc.feature.ipos[[1]],
-                              "preproc.feature.age" = preproc_hp_set$preproc.feature.age[[1]],
-                              "preproc.feature.akps" = preproc_hp_set$preproc.feature.akps[[1]])
+    current_preproc_hp = list("preproc.target" = preproc_hp_searchspace$preproc.target[[1]],
+                              "preproc.drop.targetout" = preproc_hp_searchspace$preproc.drop.targetout[[1]],
+                              "preproc.drop.iposca" = preproc_hp_searchspace$preproc.drop.iposca[[1]],
+                              "preproc.feature.ipos" = preproc_hp_searchspace$preproc.feature.ipos[[1]],
+                              "preproc.feature.age" = preproc_hp_searchspace$preproc.feature.age[[1]],
+                              "preproc.feature.akps" = preproc_hp_searchspace$preproc.feature.akps[[1]])
     
     # For each preprocessing operation, generate a tree with each option where the algorithm hyperparameters 
     # are tuned using resampling    
     tree_results_list = vector("list", length = length(preproc_hp_stepopt_order))
     for(i in 1:length(preproc_hp_stepopt_order)){
       tree_results_and_hp = get_stepopt_preproc_hp_fct(current_preproc_hp = current_preproc_hp,
-                                                       preproc_hp_set = preproc_hp_set,
+                                                       preproc_hp_searchspace = preproc_hp_searchspace,
                                                        preproc_of_interest = preproc_hp_stepopt_order[i],
                                                        error_of_interest = error_of_interest,
                                                        task = task,
@@ -92,7 +92,7 @@ generate_and_eval_tree_fct = function(procedure,
     return(list("tree_results_list" = tree_results_list, "final_tree" = final_tree))
   }
   
-  else if(procedure %in% c("preproc.hp.tune_algo.hp.tune")) {
+  else if(procedure %in% c("preproc.hp.default_algo.hp.tune", "preproc.hp.tune_algo.hp.tune")) {
     # Generate learner + preprocessing pipeline ----
     learner = lrn("regr.rpart", 
                   maxdepth = rpart_hp$fixed$maxdepth, 
@@ -106,19 +106,30 @@ generate_and_eval_tree_fct = function(procedure,
                                  po("preproc.feature.akps") %>>% 
                                  po("fixfactors") %>>%
                                 learner)
-   # Set parameter space for algorithm and preprocessing hp ----
-   search_space = ps(
-     regr.rpart.cp = p_dbl(lower = rpart_hp$tuning$cp_lower, 
-                           upper = rpart_hp$tuning$cp_upper),
-     regr.rpart.minbucket = p_int(lower = rpart_hp$tuning$minbucket_lower, 
-                                  upper = rpart_hp$tuning$minbucket_upper),
-     preproc.target.option = p_fct(preproc_hp_set$preproc.target),
-     preproc.drop.targetout.option = p_fct(preproc_hp_set$preproc.drop.targetout),
-     preproc.drop.iposca.option = p_fct(preproc_hp_set$preproc.drop.iposca),
-     preproc.feature.ipos.option = p_fct(preproc_hp_set$preproc.feature.ipos),
-     preproc.feature.age.option = p_fct(preproc_hp_set$preproc.feature.age),
-     preproc.feature.akps.option = p_fct(preproc_hp_set$preproc.feature.akps)
-   ) 
+    if(procedure == "preproc.hp.default_algo.hp.tune"){
+      # Set parameter space only for algorithm (preprocessing hp = default) ----
+      search_space = ps(
+        regr.rpart.cp = p_dbl(lower = rpart_hp$tuning$cp_lower, 
+                              upper = rpart_hp$tuning$cp_upper),
+        regr.rpart.minbucket = p_int(lower = rpart_hp$tuning$minbucket_lower, 
+                                     upper = rpart_hp$tuning$minbucket_upper)
+      )
+    } else if(procedure == "preproc.hp.tune_algo.hp.tune"){
+      # Set parameter space for algorithm and preprocessing hp ----
+      search_space = ps(
+        regr.rpart.cp = p_dbl(lower = rpart_hp$tuning$cp_lower, 
+                              upper = rpart_hp$tuning$cp_upper),
+        regr.rpart.minbucket = p_int(lower = rpart_hp$tuning$minbucket_lower, 
+                                     upper = rpart_hp$tuning$minbucket_upper),
+        preproc.target.option = p_fct(preproc_hp_searchspace$preproc.target),
+        preproc.drop.targetout.option = p_fct(preproc_hp_searchspace$preproc.drop.targetout),
+        preproc.drop.iposca.option = p_fct(preproc_hp_searchspace$preproc.drop.iposca),
+        preproc.feature.ipos.option = p_fct(preproc_hp_searchspace$preproc.feature.ipos),
+        preproc.feature.age.option = p_fct(preproc_hp_searchspace$preproc.feature.age),
+        preproc.feature.akps.option = p_fct(preproc_hp_searchspace$preproc.feature.akps)
+      ) 
+    }
+
     # Get tree incl. apparent and resampling and test set error ----
    final_tree = get_tuned_hp_fct(task = task,
                      data_test = data_test,
@@ -143,19 +154,19 @@ generate_and_eval_tree_fct = function(procedure,
 
 
 # function that returns a nested list containing the values_to_evaluate where across the lists, only one data choice varies
-generate_toeval_preproc_hp_fct = function(current_preproc_hp, preproc_hp_set, preproc_of_interest){
-  toeval_preproc_hp = replicate(length(preproc_hp_set[[preproc_of_interest]]), current_preproc_hp, simplify = FALSE)
+generate_toeval_preproc_hp_fct = function(current_preproc_hp, preproc_hp_searchspace, preproc_of_interest){
+  toeval_preproc_hp = replicate(length(preproc_hp_searchspace[[preproc_of_interest]]), current_preproc_hp, simplify = FALSE)
   
   # dynamically insert value of choice of interest into options to evaluate
-  for(i in 1:length(preproc_hp_set[[preproc_of_interest]])){
-    toeval_preproc_hp[[i]][[preproc_of_interest]] = preproc_hp_set[[preproc_of_interest]][i]
+  for(i in 1:length(preproc_hp_searchspace[[preproc_of_interest]])){
+    toeval_preproc_hp[[i]][[preproc_of_interest]] = preproc_hp_searchspace[[preproc_of_interest]][i]
   }
   return(toeval_preproc_hp)
 }
 
 
 get_stepopt_preproc_hp_fct = function(current_preproc_hp,
-                                             preproc_hp_set,
+                                             preproc_hp_searchspace,
                                              preproc_of_interest,
                                              error_of_interest,
                                              task,
@@ -165,7 +176,7 @@ get_stepopt_preproc_hp_fct = function(current_preproc_hp,
                                              tuning_parameters){
   # For each preprocessing operation, generate list of possible hp values that will be evaluated ----
   toeval_preproc_hp = generate_toeval_preproc_hp_fct(current_preproc_hp = current_preproc_hp,
-                                                     preproc_hp_set = preproc_hp_set,
+                                                     preproc_hp_searchspace = preproc_hp_searchspace,
                                                      preproc_of_interest = preproc_of_interest)
   
   # Generate learner incl. preprocessing pipeline with set preprocessing parameters (fixed for algorithm hp tuning) ----
