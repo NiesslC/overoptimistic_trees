56bc7adf352648eef55665769e43c3cf04207bcc
Christina Niessl <christina.niessl@gmx.de>
2024-01-18 10:28:32 +0100

    Add script to process results

---
+++ 02_code/03_process_results.R
@@ -0,0 +1,5 @@
+## results aufbereiten als datensatz
+# 5. Extract errors [(i) reported error and (ii) test set error]
+
+# check that correct vars have been used via
+#graph_learner$model$lrn_glmertree_if$model$formula
