27a2af12cbe09b3e388746c9b01cff43c1259dfa
Christina Niessl <christina.niessl@gmx.de>
2024-01-19 13:38:56 +0100

    no relevant changes

--- 02_code/03_process_results.R
+++ 02_code/03_process_results.R
@@ -1,5 +1,8 @@
 ## results aufbereiten als datensatz
-# 5. Extract errors [(i) reported error and (ii) test set error]
+#  Extract errors [(i) reported error and (ii) test set error]
 
 # check that correct vars have been used via
 #graph_learner$model$lrn_glmertree_if$model$formula
+t =res_sapv_p1[[1]]$graph_learner_tuned
+t$graph_learner_tuned$param_set
+t$model$lrn_rpart$train_task
