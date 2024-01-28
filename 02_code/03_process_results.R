db27c890ef661ba083618ce2eb1dca04491714b4
Christina Niessl <christina.niessl@gmx.de>
2024-01-28 19:12:55 +0100

    just added some comments on what to do here

--- 02_code/03_process_results.R
+++ 02_code/03_process_results.R
@@ -1,8 +1,7 @@
-## results aufbereiten als datensatz
-#  Extract errors [(i) reported error and (ii) test set error]
-
-# check that correct vars have been used via
-#graph_learner$model$lrn_glmertree_if$model$formula
+# - Check consistency of estimates of optimistic bias
+# - Extract errors [(i) reported error and (ii) test set error] (results aufbereiten als datensatz)
+# - check that correct vars have been used via graph_learner$model$lrn_glmertree_if$model$formula
 t =res_sapv_p1[[1]]$graph_learner_tuned
 t$graph_learner_tuned$param_set
 t$model$lrn_rpart$train_task
+
