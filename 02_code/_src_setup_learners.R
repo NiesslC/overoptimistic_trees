cd8eca88e29e4213fd3356dcdc3afb38edcfb342
Christina Niessl <christina.niessl@gmx.de>
2024-05-08 17:30:36 +0200

    remove reemctree (gives error)

--- 02_code/_src_setup_learners.R
+++ 02_code/_src_setup_learners.R
@@ -7,9 +7,6 @@ lrn_lmertree_t = lrn("regr.glmertree", id = "lrn_lmertree_t")
 lrn_reemtree_it = lrn("regr.reemtree", id = "lrn_reemtree_it")
 lrn_reemtree_i = lrn("regr.reemtree",  id = "lrn_reemtree_i")
 lrn_reemtree_t = lrn("regr.reemtree", id = "lrn_reemtree_t")
-lrn_reemctree_it = lrn("regr.reemctree", id = "lrn_reemctree_it")
-lrn_reemctree_i = lrn("regr.reemctree", id = "lrn_reemctree_i")
-lrn_reemctree_t = lrn("regr.reemctree", id = "lrn_reemctree_t")
  
 # Set up learner hyperparameters (fixed and tunable) ----
 cp_lower = 0.001
@@ -103,27 +100,6 @@ lrn_reemtree_t.hp$fixed$random =  ~1|team_id
 lrn_reemtree_t.hp$tunable = tunable_hp_ps.cp_minbucket$clone()
 
 
-## lrn_reemctree_it
-lrn_reemctree_it.hp = list(
-  fixed = list( 
-    include.partitioning.vars_expr = include.partitioning.vars_expr,
-    random = ~1|team_id/companion_id,
-    maxdepth = maxdepth,
-    minsplit = minsplit),
-  tunable = tunable_hp_ps.mincriterion_minbucket$clone()
-)
-## lrn_reemctree_i
-lrn_reemctree_i.hp = lrn_reemctree_it.hp
-lrn_reemctree_i.hp$fixed$random =  ~1|companion_id
-lrn_reemctree_i.hp$tunable = tunable_hp_ps.mincriterion_minbucket$clone()
-
-
-## lrn_reemctree_t
-lrn_reemctree_t.hp = lrn_reemctree_it.hp
-lrn_reemctree_t.hp$fixed$random =  ~1|team_id
-lrn_reemctree_t.hp$tunable = tunable_hp_ps.mincriterion_minbucket$clone()
-
-
 
 # Assign fixed hyperparameter values to learners ----
 lrn_cart$param_set$values = lrn_cart.hp$fixed
@@ -134,19 +110,16 @@ lrn_lmertree_t$param_set$values = lrn_lmertree_t.hp$fixed
 lrn_reemtree_it$param_set$values = lrn_reemtree_it.hp$fixed
 lrn_reemtree_i$param_set$values = lrn_reemtree_i.hp$fixed
 lrn_reemtree_t$param_set$values = lrn_reemtree_t.hp$fixed
-lrn_reemctree_it$param_set$values = lrn_reemctree_it.hp$fixed
-lrn_reemctree_i$param_set$values = lrn_reemctree_i.hp$fixed
-lrn_reemctree_t$param_set$values = lrn_reemctree_t.hp$fixed
 
 
 # Generate list of learners with assigned fixed hps ----
 learners_default = list(lrn_cart,lrn_ctree,lrn_lmertree_it,lrn_lmertree_i,lrn_lmertree_t, lrn_reemtree_it,
-                     lrn_reemtree_i,lrn_reemtree_t,lrn_reemctree_it,lrn_reemctree_i,lrn_reemctree_t)
+                     lrn_reemtree_i,lrn_reemtree_t)
 names(learners_default) = c("lrn_cart","lrn_ctree","lrn_lmertree_it","lrn_lmertree_i","lrn_lmertree_t", "lrn_reemtree_it",
-                         "lrn_reemtree_i","lrn_reemtree_t","lrn_reemctree_it","lrn_reemctree_i","lrn_reemctree_t")
+                         "lrn_reemtree_i","lrn_reemtree_t")
 
-# only use subset of all initially specified learners (not those with _it and _i) -> 5 learners in total
-learners_default = learners_default[c("lrn_cart","lrn_ctree","lrn_lmertree_t","lrn_reemtree_t","lrn_reemctree_t")]
+# only use subset of all initially specified learners (not those with _it and _i) -> 4 learners in total
+learners_default = learners_default[c("lrn_cart","lrn_ctree","lrn_lmertree_t","lrn_reemtree_t")]
 
 # Generate list of search spaces for tunable hps ----
 lrn_cart.hp$tunable$set_id = "lrn_cart"
@@ -157,9 +130,6 @@ lrn_lmertree_t.hp$tunable$set_id = "lrn_lmertree_t"
 lrn_reemtree_it.hp$tunable$set_id = "lrn_reemtree_it"
 lrn_reemtree_i.hp$tunable$set_id = "lrn_reemtree_i"
 lrn_reemtree_t.hp$tunable$set_id = "lrn_reemtree_t"
-lrn_reemctree_it.hp$tunable$set_id = "lrn_reemctree_it"
-lrn_reemctree_i.hp$tunable$set_id = "lrn_reemctree_i"
-lrn_reemctree_t.hp$tunable$set_id = "lrn_reemctree_t"
 
 learners_hp_searchspace_default = paste0(names(learners_default),".hp") %>% 
   purrr::map(~ ParamSetCollection$new(list(get(.x)$tunable)))
@@ -171,10 +141,10 @@ rm(# parameters
    "minbucket_lower","minbucket_upper","mincriterion_lower","mincriterion_upper",
    "alpha_lower","alpha_upper","cp_lower","cp_upper", "include.partitioning.vars_expr","maxdepth","minsplit",
    # hp lists
-   "lrn_ctree.hp","lrn_lmertree_t.hp","lrn_lmertree_i.hp","lrn_lmertree_it.hp","lrn_reemctree_t.hp","lrn_reemctree_i.hp",
-   "lrn_reemctree_it.hp","lrn_reemtree_t.hp","lrn_reemtree_i.hp","lrn_reemtree_it.hp","lrn_cart.hp",
+   "lrn_ctree.hp","lrn_lmertree_t.hp","lrn_lmertree_i.hp","lrn_lmertree_it.hp",
+  "lrn_reemtree_t.hp","lrn_reemtree_i.hp","lrn_reemtree_it.hp","lrn_cart.hp",
    # individual learners
-   "lrn_ctree", "lrn_lmertree_t","lrn_lmertree_i","lrn_lmertree_it","lrn_reemctree_t","lrn_reemctree_i",
-   "lrn_reemctree_it","lrn_reemtree_t","lrn_reemtree_i","lrn_reemtree_it","lrn_cart")
+   "lrn_ctree", "lrn_lmertree_t","lrn_lmertree_i","lrn_lmertree_it",
+  "lrn_reemtree_t","lrn_reemtree_i","lrn_reemtree_it","lrn_cart")
 
 
